1156958ce7ecd57320b3d25e06403c8888a18388
Alexander Ivanishko <ivanishco@mail.ru>
2019-03-11 14:41:48 +0300

    Create select.php

---
+++ admin/select.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+
+
+
+
+?>
+
+
+
+
+<form action="" method="post">
+    <select name="playerselect" id="">
+        <option value="Веркашанский">Веркашанский</option>
+        <option value="Иванов">Иванов</option>
+    </select>
+    <input type="submit" name="submit">
+</form>
+
