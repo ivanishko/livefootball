7b5949159a0d80d6f324dccbe4530544369c2f94
Alexander Ivanishko <ivanishco@mail.ru>
2019-03-11 22:35:01 +0300

    Update select.php

--- admin/select.php
+++ admin/select.php
@@ -1,21 +1,17 @@
 <?php
+if (count($_POST) > 0){
 
     echo "<pre>";
+    echo "hello world!";
     var_dump($_POST);
     echo "</pre>";
-
-
-
-
+}
 ?>
-
-
-
 
 <form action="" method="post">
     <select name="playerselect" id="">
-        <option value="Веркашанский">Веркашанский</option>
-        <option value="Иванов">Иванов</option>
+        <option value="player1">Веркашанский</option>
+        <option value="player2">Иванов</option>
     </select>
     <input type="submit" name="submit">
 </form>
